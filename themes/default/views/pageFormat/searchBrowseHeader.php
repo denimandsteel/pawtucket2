b0c8e57a2e33286e05f56c4d1281774d63a55ba8
Chris Harris <christopherharris@fastmail.fm>
2021-05-25 13:21:44 -0700

    Added shared header for front and browse pages

---
+++ themes/default/views/pageFormat/searchBrowseHeader.php
@@ -0,0 +1,4 @@
+      <h1>Online Collections</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate, orci quis vehicula eleifend, metus elit laoreet elit.</p>
+      <h2 <?php print ($this->request->getController() == "Front") ? 'class="active"' : ''; ?>><?php print caNavLink($this->request, _t("Search + Explore"), "", "", "Front", "index"); ?></h2>
+      <h2 <?php print ($this->request->getController() == "Browse") ? 'class="active"' : ''; ?>><?php print caNavLink($this->request, _t("Browse"), "", "", "Browse", "index"); ?></h2>
