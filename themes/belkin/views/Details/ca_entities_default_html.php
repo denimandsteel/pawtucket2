d579fa87e7751ef8ac2117217af1e75fe6334dab
Scott Caldwell <scottc.caldwell@gmail.com>
2021-07-05 09:22:56 -0700

    updated artist detail page

--- themes/belkin/views/Details/ca_entities_default_html.php
+++ themes/belkin/views/Details/ca_entities_default_html.php
@@ -14,9 +14,7 @@
   <div class="detail-info">
     <div class="detail-info-intro">
       <div class="container">
-        {{{<ifdef code="^ca_entities.preferred_labels.displayname">
-            <h1>^ca_entities.preferred_labels.displayname</h1>
-        </ifdef>}}}
+        {{{<h1>^ca_entities.preferred_labels.displayname</h1>}}}
         <dl class="detail-info-list">
         
           {{{<ifdef code="ca_entities.vital_dates_ind.vital_date_ind">
@@ -27,20 +25,20 @@
 					
 					{{{<ifcount code="ca_collections" min="1" max="1"><dt>Related collection</dt></ifcount>}}}
 					{{{<ifcount code="ca_collections" min="2"><dt>Related collections</dt></ifcount>}}}
-					{{{<unit relativeTo="ca_entities_x_collections" delimiter="<br/>"><unit relativeTo="ca_collections"><l><dd>^ca_collections.preferred_labels.name</dd></l> (^relationship_typename)</unit></unit>}}}
+					{{{<unit relativeTo="ca_entities_x_collections" delimiter="<br/>"><unit relativeTo="ca_collections"><l><dd class="detail-related-link">^ca_collections.preferred_labels.name</dd></l></unit></unit>}}}
 
 					
-					{{{<ifcount code="ca_entities.related" min="1" max="1"><dt>Related person</dt></ifcount>}}}
+					<!-- {{{<ifcount code="ca_entities.related" min="1" max="1"><dt>Related person</dt></ifcount>}}}
 					{{{<ifcount code="ca_entities.related" min="2"><dt>Related people</dt></ifcount>}}}
-					{{{<unit relativeTo="ca_entities_x_entities" delimiter="<br/>"><unit relativeTo="ca_entities" delimiter="<br/>"><l><dd>^ca_entities.related.preferred_labels.displayname</dd></l></unit> (^relationship_typename)</unit>}}}
-					
+					{{{<unit relativeTo="ca_entities_x_entities" delimiter="<br/>"><unit relativeTo="ca_entities" delimiter="<br/>"><l><dd class="detail-related-link">^ca_entities.related.preferred_labels.displayname</dd></l></unit></unit>}}}
+					 -->
 					{{{<ifcount code="ca_occurrences" min="1" max="1"><dt>Related exhibition</dt></ifcount>}}}
 					{{{<ifcount code="ca_occurrences" min="2"><dt>Related exhibitions</dt></ifcount>}}}
-					{{{<unit relativeTo="ca_entities_x_occurrences" delimiter="<br/>"><unit relativeTo="ca_occurrences" delimiter="<br/>"><l><dd>^ca_occurrences.preferred_labels.name</dd></l></unit> (^relationship_typename)</unit>}}}
+					{{{<unit relativeTo="ca_entities_x_occurrences" delimiter="<br/>"><unit relativeTo="ca_occurrences" delimiter="<br/>"><l><dd class="detail-related-link">^ca_occurrences.preferred_labels.name</dd></l></unit></unit>}}}
 					
-					{{{<ifcount code="ca_places" min="1" max="1"><dt>Related place</dt></ifcount>}}}
+					<!-- {{{<ifcount code="ca_places" min="1" max="1"><dt>Related place</dt></ifcount>}}}
 					{{{<ifcount code="ca_places" min="2"><dt>Related places</dt></ifcount>}}}
-					{{{<unit relativeTo="ca_entities_x_places" delimiter="<br/>"><unit relativeTo="ca_places" delimiter="<br/>"><l><dd>^ca_places.preferred_labels.name</dd></l></unit> (^relationship_typename)</unit>}}}		
+					{{{<unit relativeTo="ca_entities_x_places" delimiter="<br/>"><unit relativeTo="ca_places" delimiter="<br/>"><l><dd class="detail-related-link">^ca_places.preferred_labels.name</dd></l></unit></unit>}}}		 -->
 
 
         </dl>
@@ -48,7 +46,7 @@
     </div>
 
     
-    {{{<ifdef code="ca_entities.description">
+    {{{<ifdef code="ca_entities.biography">
     <div class="detail-info-box fw-border-top accordion">
       <div class="container">
         <div class="detail-info-box-header">
@@ -56,7 +54,7 @@
           <button class="button accordion-toggle">Hide</button>
         </div>
           <p class="accordion-details" aria-expanded="true">
-					 <div class='unit'>^ca_entities.description</div>
+					 <div class='unit detail-info-paragraph'>^ca_entities.biography</div>
               
           </p>
       </div>
@@ -64,11 +62,22 @@
     </ifdef>}}}
 
     {{{<ifcount code="ca_objects" min="2">
-			<div class="row">
-				<div id="browseResultsContainer">
-					<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>
-				</div><!-- end browseResultsContainer -->
-			</div><!-- end row -->
+    <div class="detail-info-box fw-border-top accordion">
+      <div class="container">
+        <div class="detail-info-box-header">
+          <h2>Related Records</h2>
+          <button class="button accordion-toggle">Hide</button>
+        </div>
+      </div>
+      <div class="accordion-details" aria-expanded="true">
+        <div class="container">
+          <div class="result-objects result-objects--grid">
+            <div id="browseResultsContainer" class="container">
+              <?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>
+            </div><!-- end browseResultsContainer -->
+          </div>
+        </div><!-- end row -->
+      </div>
 			<script type="text/javascript">
 				jQuery(document).ready(function() {
 					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'entity_id:^ca_entities.entity_id'), array('dontURLEncodeParameters' => true)); ?>", function() {
