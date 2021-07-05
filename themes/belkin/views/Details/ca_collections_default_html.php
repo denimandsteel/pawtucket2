27e4f140fc47c02c797ccd524f34adc7c1f7ce5e
Scott Caldwell <scottc.caldwell@gmail.com>
2021-07-05 09:23:42 -0700

    collections view page

--- themes/belkin/views/Details/ca_collections_default_html.php
+++ themes/belkin/views/Details/ca_collections_default_html.php
@@ -15,112 +15,187 @@
 	$vn_top_level_collection_id = array_shift($t_item->get('ca_collections.hierarchy.collection_id', array("returnWithStructure" => true)));
 
 ?>
-<div class="row">
-	<div class='col-xs-12 navTop'><!--- only shown at small screen size -->
-		{{{previousLink}}}{{{resultsLink}}}{{{nextLink}}}
-	</div><!-- end detailTop -->
-	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
-		<div class="detailNavBgLeft">
-			{{{previousLink}}}{{{resultsLink}}}
-		</div><!-- end detailNavBgLeft -->
-	</div><!-- end col -->
-	<div class='col-xs-12 col-sm-10 col-md-10 col-lg-10'>
-		<div class="container">
-			<div class="row">
-				<div class='col-md-12 col-lg-12'>
-					<H1>{{{^ca_collections.preferred_labels.name}}}</H1>
-					<H2>{{{^ca_collections.type_id}}}{{{<ifdef code="ca_collections.idno">, ^ca_collections.idno</ifdef>}}}</H2>
-					{{{<ifdef code="ca_collections.parent_id"><div class="unit">Part of: <unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></div></ifdef>}}}
-<?php					
-					if ($vn_pdf_enabled) {
-						print "<div class='exportCollection'><span class='glyphicon glyphicon-file' aria-label='"._t("Download")."'></span> ".caDetailLink($this->request, "Download as PDF", "", "ca_collections",  $vn_top_level_collection_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_collections_summary'))."</div>";
-					}
-?>
-				</div><!-- end col -->
-			</div><!-- end row -->
-			<div class="row">
-				<div class='col-sm-12'>
-<?php
-			if ($vb_show_hierarchy_viewer) {	
-?>
-				<div id="collectionHierarchy"><?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?></div>
-				<script>
-					$(document).ready(function(){
-						$('#collectionHierarchy').load("<?php print caNavUrl($this->request, '', 'Collections', 'collectionHierarchy', array('collection_id' => $t_item->get('collection_id'))); ?>"); 
-					})
-				</script>
-<?php				
-			}									
-?>				
-				</div><!-- end col -->
-			</div><!-- end row -->
-			<div class="row">			
-				<div class='col-md-6 col-lg-6'>
-					{{{<ifdef code="ca_collections.description"><label>About</label>^ca_collections.description<br/></ifdef>}}}
-					{{{<ifcount code="ca_objects" min="1" max="1"><div class='unit'><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.large</l><div class='caption'>Related Object: <l>^ca_objects.preferred_labels.name</l></div></unit></div></ifcount>}}}
-<?php
-				# Comment and Share Tools
-				if ($vn_comments_enabled | $vn_share_enabled) {
-						
-					print '<div id="detailTools">';
-					if ($vn_comments_enabled) {
-?>				
-						<div class="detailTool"><a href='#' onclick='jQuery("#detailComments").slideToggle(); return false;'><span class="glyphicon glyphicon-comment" aria-label="<?php print _t("Comments and tags"); ?>"></span>Comments (<?php print sizeof($va_comments); ?>)</a></div><!-- end detailTool -->
-						<div id='detailComments'><?php print $this->getVar("itemComments");?></div><!-- end itemComments -->
-<?php				
-					}
-					if ($vn_share_enabled) {
-						print '<div class="detailTool"><span class="glyphicon glyphicon-share-alt" aria-label="'._t("Share").'"></span>'.$this->getVar("shareLink").'</div><!-- end detailTool -->';
-					}
-					print '</div><!-- end detailTools -->';
-				}				
-?>
-					
-				</div><!-- end col -->
-				<div class='col-md-6 col-lg-6'>
-					{{{<ifcount code="ca_collections.related" min="1" max="1"><label>Related collection</label></ifcount>}}}
+
+
+
+					<!-- {{{<ifdef code="ca_collections.description"><label>About</label>^ca_collections.description<br/></ifdef>}}}
+					{{{<ifcount code="ca_objects" min="1" max="1"><div class='unit'><unit relativeTo="ca_objects" delimiter=" "><l>^ca_object_representations.media.large</l><div class='caption'>Related Object: <l>^ca_objects.preferred_labels.name</l></div></unit></div></ifcount>}}} -->
+
+
+					<!-- {{{<ifcount code="ca_collections.related" min="1" max="1"><label>Related collection</label></ifcount>}}}
 					{{{<ifcount code="ca_collections.related" min="2"><label>Related collections</label></ifcount>}}}
 					{{{<unit relativeTo="ca_collections_x_collections"><unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.related.preferred_labels.name</l></unit> (^relationship_typename)</unit>}}}
 					
-					{{{<ifcount code="ca_entities" min="1" max="1"><label>Related person</label></ifcount>}}}
-					{{{<ifcount code="ca_entities" min="2"><label>Related people</label></ifcount>}}}
-					{{{<unit relativeTo="ca_entities_x_collections"><unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit> (^relationship_typename)</unit>}}}
-					
+
 					{{{<ifcount code="ca_occurrences" min="1" max="1"><label>Related occurrence</label></ifcount>}}}
 					{{{<ifcount code="ca_occurrences" min="2"><label>Related occurrences</label></ifcount>}}}
 					{{{<unit relativeTo="ca_occurrences_x_collections"><unit relativeTo="ca_occurrences" delimiter="<br/>"><l>^ca_occurrences.preferred_labels.name</l></unit> (^relationship_typename)</unit>}}}
+
+ -->
+
+
+<article class="detail">
+  <nav class="detail-nav">
+    <a class="link link--back" href="">Results</a>
+    <div class="detail-breadcrumb">
+      {{{<ifdef code="ca_collections.parent_id"><div class="unit">Part of: <unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></div></ifdef>}}}
+    </div>
+
+  </nav>
+
+
+  <div class="detail-info">
+    <div class="detail-info-intro">
+      <div class="container">
+        {{{<h1>^ca_collections.preferred_labels.name</h1>}}}
+
+        <dl class="detail-info-list">
+          <dt>Catalogue Number</dt>
+					{{{<ifdef code="ca_collections.idno"><dd> ^ca_collections.idno </dd></ifdef>}}}
+					{{{<ifnotdef code="ca_collections.idno"><dd>–</dd></ifnotdef>}}}
+					<!-- {{{<ifdef code="ca_collections.parent_id"><div class="unit">Part of: <unit relativeTo="ca_collections.hierarchy" delimiter=" &gt; "><l>^ca_collections.preferred_labels.name</l></unit></div></ifdef>}}} -->
+				
+          {{{<ifcount code="ca_entities" min="1" max="1"><dt>Artist/Creator</dt></ifcount>}}}
+					{{{<ifcount code="ca_entities" min="2"><dt>Artist/Creators</dt></ifcount>}}}
+					{{{<dd><unit relativeTo="ca_entities_x_collections"><unit relativeTo="ca_entities" delimiter="<br/>"><l>^ca_entities.preferred_labels.displayname</l></unit> (^relationship_typename)</unit></dd>}}}
 					
-					{{{<ifcount code="ca_places" min="1" max="1"><label>Related place</label></ifcount>}}}
-					{{{<ifcount code="ca_places" min="2"><label>Related places</label></ifcount>}}}
-					{{{<unit relativeTo="ca_places_x_collections"><unit relativeTo="ca_places" delimiter="<br/>"><l>^ca_places.preferred_labels.name</l></unit> (^relationship_typename)</unit>}}}					
-				</div><!-- end col -->
-			</div><!-- end row -->
-{{{<ifcount code="ca_objects" min="2">
-			<div class="row">
-				<div id="browseResultsContainer">
-					<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>
-				</div><!-- end browseResultsContainer -->
-			</div><!-- end row -->
-			<script type="text/javascript">
-				jQuery(document).ready(function() {
-					jQuery("#browseResultsContainer").load("<?php print caNavUrl($this->request, '', 'Search', 'objects', array('search' => 'collection_id:^ca_collections.collection_id'), array('dontURLEncodeParameters' => true)); ?>", function() {
-						jQuery('#browseResultsContainer').jscroll({
-							autoTrigger: true,
-							loadingHtml: '<?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?>',
-							padding: 20,
-							nextSelector: 'a.jscroll-next'
-						});
-					});
-					
-					
-				});
-			</script>
-</ifcount>}}}
-		</div><!-- end container -->
-	</div><!-- end col -->
-	<div class='navLeftRight col-xs-1 col-sm-1 col-md-1 col-lg-1'>
-		<div class="detailNavBgRight">
-			{{{nextLink}}}
-		</div><!-- end detailNavBgLeft -->
-	</div><!-- end col -->
-</div><!-- end row -->
+          <dt>Date</dt>
+          {{{<ifdef code="ca_collections.search_date">
+            <dd>^ca_collections.search_date</dd>
+          </ifdef>
+          <ifnotdef code="ca_collections.search_date">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+
+          <dt>Identifier</dt>
+          {{{<ifdef code="ca_collections.alt_idno">
+            <dd>^ca_collections.alt_idno</dd>
+          </ifdef>
+          <ifnotdef code="ca_collections.alt_idno">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+
+          <dt>Level of Description</dt>
+          {{{<ifdef code="ca_collections.level_description">
+            <dd>^ca_collections.level_description</dd>
+          </ifdef>
+          <ifnotdef code="ca_collections.level_description">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+    
+        </dl>
+      </div>
+    </div>
+        
+    {{{<ifdef code="ca_collections.RAD_scopecontent">
+    <div class="detail-info-box fw-border-top accordion">
+      <div class="container">
+        <div class="detail-info-box-header">
+          <h2>Scope & Content</h2>
+          <button class="button accordion-toggle">Hide</button>
+        </div>
+          <p class="accordion-details" aria-expanded="true">
+					 <div class='unit detail-info-paragraph'>^ca_collections.RAD_scopecontent</div>
+          </p>
+      </div>
+    </div>
+    </ifdef>}}}
+
+    <div class="detail-info-box fw-border-top accordion">
+      <div class="container">
+        <div class="detail-info-box-header">
+          <h2>Physical Description</h2>
+          <button class="button accordion-toggle">Hide</button>
+        </div>
+        <dl class="detail-info-list accordion-details" aria-expanded="true">
+        <!-- Is description different than phyiscal extent?? -->
+          <dt>Physical Extent</dt>
+          {{{<ifdef code="ca_collections.RAD_extent">
+            <dd>^ca_collections.RAD_extent</dd>
+          </ifdef>
+          <ifnotdef code="ca_collections.RAD_extent">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+
+          <dt>GMD</dt>
+          {{{<ifdef code="ca_collections.gmd">
+            <dd>^ca_collections.gmd</dd>
+          </ifdef>
+          <ifnotdef code="ca_collections.gmd">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+
+        </dl>
+      </div>
+    </div>
+
+    <!-- <div class="detail-info-box fw-border-top accordion">
+      <div class="container">
+        <div class="detail-info-box-header">
+          <h2>Collection History</h2>
+          <button class="button accordion-toggle">Hide</button>
+        </div>
+        <dl class="detail-info-list accordion-details" aria-expanded="true">
+
+          <dt>Collection</dt>
+          {{{
+          <ifdef code="ca_collections">
+            <unit relativeTo="ca_collections" delimiter="<br/>"><l><dd>^ca_collections.preferred_labels.name</dd></l></unit>
+          </ifdef>
+          <ifnotdef code="ca_collections"><dd>–</dd></ifnotdef>
+        }}}
+
+          <dt>Credit Line</dt>
+          {{{<ifdef code="ca_collections.credit_line">
+            <dd>^ca_collections.credit_line</dd>
+          </ifdef>
+          <ifnotdef code="ca_collections.credit_line">
+            <dd>–</dd>
+          </ifnotdef>}}}
+  
+  
+          <dt>Related exhibition</dt>
+          {{{
+            <ifdef code="ca_occurences">
+              <unit relativeTo="ca_occurences_x_collections" delimiter="<br/>"><l><dd>^ca_occurences.preferred_labels.name</dd></l></unit>
+            </ifdef>
+            <ifnotdef code="ca_occurences"><dd>–</dd></ifnotdef>
+          }}}
+
+          <!-- TODO: figure out what this actually is -->
+          <!-- <dt>Exhibition History</dt>
+          {{{
+            <ifdef code="^ca_occurences.preferred_labels.name"><unit relativeTo="ca_occurences" delimiter="<br/>"><l><dd>^ca_occurences.preferred_labels.name</dd></l></unit></ifdef>
+            <ifnotdef code="^ca_occurences.preferred_labels.name"><dd>–</dd></ifnotdef>
+          }}} 
+        </dl>
+      </div>
+    </div> -->
+
+    <div class="detail-info-box fw-border-top accordion">
+      <div class="container">
+        <div class="detail-info-box-header">
+          <h2>Navigate Fonds</h2>
+          <button class="button accordion-toggle">Hide</button>
+        </div>
+          <div class="accordion-details" aria-expanded="true">
+            <div id="collectionHierarchy"><?php print caBusyIndicatorIcon($this->request).' '.addslashes(_t('Loading...')); ?></div>
+            <script>
+              $(document).ready(function(){
+                $('#collectionHierarchy').load("<?php print caNavUrl($this->request, '', 'Collections', 'collectionHierarchy', array('collection_id' => $t_item->get('collection_id'))); ?>"); 
+              })
+            </script>
+          </div>
+      </div>
+    </div>
+    
+    <div class="detail-actions fw-border-top">
+      <div class="container">
+        <button class="button button--catalogue "><?php print caDetailLink($this->request, "Download as PDF", "", "ca_collections",  $vn_top_level_collection_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_collections_summary')); ?></button>
+        <button class="button button--catalogue ">Contact us</button>
+        <button class="button button--catalogue "><?php print $this->getVar("shareLink"); ?></button>
+      </div>
+    </div>
+
+    <!--  -->
+</article>
