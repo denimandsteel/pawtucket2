cd825dfcb4e4ddf48a882298f892e2d7262c0fd2
Scott Caldwell <scottc.caldwell@gmail.com>
2021-07-05 09:22:19 -0700

    Object view page updated to show more fields, and handle empty fields

--- themes/belkin/views/Details/ca_objects_default_html.php
+++ themes/belkin/views/Details/ca_objects_default_html.php
@@ -40,8 +40,6 @@
     <a class="link link--back" href="">Results</a>
     <div class="detail-breadcrumb">{{{<unit relativeTo="ca_collections" delimiter="<br/>"><l>^ca_collections.preferred_labels.name</l></unit><ifcount min="1" code="ca_collections"> / </ifcount>}}}{{{ca_objects.preferred_labels.name}}}</div>
   </nav>
-  <!-- what if there are multiple images? generate/style carousel?? -->
-  <!-- if image --> 
 
   <div class="detail-images">
     <div class="container">
@@ -68,29 +66,36 @@
   <div class="detail-info">
     <div class="detail-info-intro fw-border-top">
       <div class="container">
-        <span class="detail-label">Object</span>
+        {{{<ifdef code="ca_objects.catalogue_destination">
+            <span class="detail-label">^ca_objects.catalogue_destination.preferred_labels</span>
+        </ifdef>}}}
         {{{<ifdef code="ca_objects.preferred_labels.name">
             <h1>^ca_objects.preferred_labels.name</h1>
         </ifdef>}}}
         <dl class="detail-info-list">
-          {{{<ifcount restrictToRelationshipTypes="creator" code="ca_entities" min="1" max="1"><dt>Artist/Creator:</dt></ifcount>}}}
-          {{{<ifcount restrictToRelationshipTypes="creator" code="ca_entities" min="2"><dt>Artists/Creators:</dt></ifcount>}}}
+          {{{<ifcount restrictToRelationshipTypes="creator, artist" code="ca_entities" min="1" max="1"><dt>Artist/Creator</dt></ifcount>}}}
+          {{{<ifcount restrictToRelationshipTypes="creator, artist" code="ca_entities" min="2"><dt>Artists/Creators</dt></ifcount>}}}
 
-          {{{<unit restrictToRelationshipTypes="creator" relativeTo="ca_objects_x_entities" delimiter="<br/>"><unit restrictToRelationshipTypes="creator" relativeTo="ca_entities"><l>
+          {{{<unit restrictToRelationshipTypes="creator, artist" relativeTo="ca_objects_x_entities" delimiter="<br/>"><unit restrictToRelationshipTypes="creator, artist" relativeTo="ca_entities"><l>
             <dd>^ca_entities.preferred_labels</dd>
           </l>}}}
 
+          <dt>Date</dt>
+          {{{<ifdef code="ca_objects.search_date">
+            <dd>^ca_objects.search_date</dd>
+          </ifdef>
+          <ifnotdef code="ca_objects.search_date">
+            <dd>–</dd>
+          </ifnotdef>}}} 
 
-          {{{<ifdef code="ca_objects.pub_date">
-            <dt>Date:</dt>
-            <dd>^ca_objects.pub_date</dd>
-          </ifdef>}}} 
-
-
+          <dt>ID #</dt>
           {{{<ifdef code="ca_objects.idno">
-            <dt>Object Number:</dt>
             <dd>^ca_objects.idno</dd>
-          </ifdef>}}}
+          </ifdef>
+          <ifnotdef code="ca_objects.idno">
+            <dd>–</dd>
+          </ifnotdef>}}}
+            
         </dl>
       </div>
     </div>
@@ -103,26 +108,47 @@
         </div>
         <dl class="detail-info-list accordion-details" aria-expanded="true">
         <!-- Is description different than phyiscal extent?? -->
-          {{{<ifdef code="ca_objects.description">
-            <dt class='unit'>Description</dt>
-            <dd class="trimText">^ca_objects.description</dd>
-          </ifdef>}}}
-          {{{<ifdef code="ca_objects.custom_extent">
-            <dt>Physical Extent:</dt>
-            <dd>^ca_objects.custom_extent</dd>
-          </ifdef>}}}
-          {{{<ifdef code="ca_objects.artwork_object">
-            <dt>Object type:</dt>
-            <dd>^ca_objects.artwork_object</dd>
-          </ifdef>}}}
+          <dt>Medium</dt>
           {{{<ifdef code="ca_objects.medium">
-            <dt>Medium:</dt>
             <dd>^ca_objects.medium</dd>
-          </ifdef>}}}
+          </ifdef>
+          <ifnotdef code="ca_objects.medium">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+
+          <dt>Support</dt>
+          {{{<ifdef code="ca_objects.support">
+            <dd>^ca_objects.support</dd>
+          </ifdef>
+          <ifnotdef code="ca_objects.support">
+            <dd>–</dd>
+          </ifnotdef>}}} 
+
+          <!-- ONLY FOR ARTWORKS - add condition? -->
+          <dt>Dimensions</dt>
+          <dd>
+            {{{<ifdef code="ca_objects.dimensions.height">h- ^ca_objects.dimensions.height</ifdef>
+            <ifdef code="ca_objects.dimensions.width">w- ^ca_objects.dimensions.width</ifdef> 
+            <ifdef code="ca_objects.dimensions.depth">d- ^ca_objects.dimensions.depth </ifdef>
+            <ifdef code="ca_objects.dimensions.width|ca_objects.dimensions.height|ca_objects.dimensions.depth"> cm</ifdef>
+            <ifnotdef code="ca_objects.dimensions.width|ca_objects.dimensions.height|ca_objects.dimensions.depth">–</ifnotdef>}}}
+          </dd>
+
+          <dt>Duration</dt>
           {{{<ifdef code="ca_objects.duration">
-            <dt>Duration:</dt>
             <dd>^ca_objects.duration</dd>
-          </ifdef>}}}
+          </ifdef>
+          <ifnotdef code="ca_objects.duration">
+            <dd>–</dd>
+          </ifnotdef>}}}
+
+          <dt class='unit'>Object Description</dt>
+          {{{<ifdef code="ca_objects.description">
+            <dd class="trimText">^ca_objects.description</dd>
+          </ifdef>
+          <ifnotdef code="ca_objects.description">
+            <dd>–</dd>
+          </ifnotdef>}}}
         </dl>
       </div>
     </div>
@@ -133,20 +159,46 @@
           <button class="button accordion-toggle">Hide</button>
         </div>
         <dl class="detail-info-list accordion-details" aria-expanded="true">
+
           <dt>Collection</dt>
-          {{{<unit relativeTo="ca_collections" delimiter="<br/>"><l><dd>^ca_collections.preferred_labels.name</dd></l></unit>}}}
+          {{{
+          <ifdef code="ca_collections">
+            <unit relativeTo="ca_collections" delimiter="<br/>"><l><dd>^ca_collections.preferred_labels.name</dd></l></unit>
+          </ifdef>
+          <ifnotdef code="ca_collections"><dd>–</dd></ifnotdef>
+        }}}
+
+          <dt>Credit Line</dt>
+          {{{<ifdef code="ca_objects.credit_line">
+            <dd>^ca_objects.credit_line</dd>
+          </ifdef>
+          <ifnotdef code="ca_objects.credit_line">
+            <dd>–</dd>
+          </ifnotdef>}}}
+  
           <dt>Related Exhibitions</dt>
-          {{{<unit relativeTo="ca_occurences" delimiter="<br/>"><l><dd>^ca_occurences.preferred_labels.name</dd></l></unit>}}}
-          <dd></dd>
+          {{{
+            <ifdef code="ca_occurences">
+              <unit relativeTo="ca_occurences" delimiter="<br/>"><l><dd>^ca_occurences.preferred_labels.name</dd></l></unit>
+            </ifdef>
+            <ifnotdef code="ca_occurences"><dd>–</dd></ifnotdef>
+          }}}
+
+          <!-- TODO: figure out what this actually is -->
+          <!-- <dt>Exhibition History</dt>
+          {{{
+            <ifdef code="^ca_occurences.preferred_labels.name"><unit relativeTo="ca_occurences" delimiter="<br/>"><l><dd>^ca_occurences.preferred_labels.name</dd></l></unit></ifdef>
+            <ifnotdef code="^ca_occurences.preferred_labels.name"><dd>–</dd></ifnotdef>
+          }}} -->
         </dl>
       </div>
       </div>
     </div>
-    <!-- <div class="detail-actions fw-border-top">
+    <div class="detail-actions fw-border-top">
       <div class="container">
-        <button class="button button--catalogue ">Export</button>
+        <button class="button button--catalogue "><?php print caDetailLink($this->request, "Export Results", "faDownload", "ca_objects",  $vn_id, array('view' => 'pdf', 'export_format' => '_pdf_ca_objects_summary')); ?></button>
         <button class="button button--catalogue ">Contact us</button>
-        <button class="button button--catalogue ">Share</button>
+        <button class="button button--catalogue "><?php print $this->getVar("shareLink"); ?></button>
       </div>
-    </div> -->
+    </div>
 </article>
