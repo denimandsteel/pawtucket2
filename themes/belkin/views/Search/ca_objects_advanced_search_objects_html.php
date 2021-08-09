fa43d5db470de3ac25db436c892060bb8a5742d0
Scott Caldwell <scottc.caldwell@gmail.com>
2021-08-09 10:24:24 -0700

    adv search - added all categories to hardcoded select, updated values for fields

--- themes/belkin/views/Search/ca_objects_advanced_search_objects_html.php
+++ themes/belkin/views/Search/ca_objects_advanced_search_objects_html.php
@@ -16,28 +16,28 @@
   </div>
   <button class="button accordion-toggle">Advanced Search</button>
 
-  <div class="advanced-search accordion-details"  style="height: 0px" aria-expanded="false">
+  <div class="advanced-search accordion-details" style="height: 0px" aria-expanded="false">
     <input type="hidden" name="_formName" value="caAdvancedSearch">
     <div class="advanced-search-fields">
       <div class="advanced-search-field">
-        <label for="ca_objects_catalogue_destination.preferred_labels" class="formLabel">Catalogue</label>
-        <select class="search-input" name="ca_objects_catalogue_destination.preferred_labels" class="" id="ca_objects_catalogue_destination.preferred_labels">
+        <label for="ca_objects.catalogue_destination.preferred_labels" class="formLabel">Catalogue</label>
+        <select class="search-input" name="ca_objects.catalogue_destination.preferred_labels" class="" id="ca_objects.catalogue_destination.preferred_labels">
           <option value="">Select Catalogue</option>
-          <option value="Archive">Archive</option>
-          <option value="Artwork">Artwork</option>
-          <option value="Library">Library</option>
+          <option value="492">Archive</option>
+          <option value="493">Artwork</option>
+          <option value="494">Library</option>
         </select>
-        <input name="ca_objects.catalogue_destination.preferred_labels_label" value="Type" type="hidden">
+        <input name="ca_objects.catalogue_destination.preferred_labels_label" value="Catalogue" type="hidden">
       </div>
       <div class="advanced-search-field">
         <label class="formLabel" for="ca_entities.preferred_labels.displayname">Artist/Creator</label>
         <input class="search-input" name="ca_entities.preferred_labels.displayname" type="text" id="ca_entities.preferred_labels.displayname"/>
-        <input name="ca_entities.preferred_labels.displayname_label" value="Display name (from related entities)" type="hidden">
+        <input name="ca_entities.preferred_labels.displayname_label" value="Artist/Creator" type="hidden">
       </div>
       <div class="advanced-search-field">
         <label class="formLabel" for="ca_objects.preferred_labels.name">Title</label>
         <input name="ca_objects.preferred_labels.name" id="ca_objects.preferred_labels.name" type="text">
-        <input name="ca_objects.preferred_labels.name_label" value="Name" type="hidden">
+        <input name="ca_objects.preferred_labels.name_label" value="Title" type="hidden">
       </div>
       <div class="advanced-search-field">
         <label class="formLabel" for="ca_objects.search_date[]">Date</label>
@@ -50,36 +50,35 @@
         <input name="ca_objects.description_label" value="Description" type="hidden">
       </div>
       <div class="advanced-search-field">
-        <label for="ca_objects_type_id" class="formLabel">Type</label>
-        <select class="search-input" name="ca_objects.type_id" class="" id="ca_objects_type_id">
+        <label for="ca_objects_object_category" class="formLabel">Type</label>
+        <select class="search-input" name="ca_objects_object_category" id="ca_objects_object_category">
           <option value="">Select Type</option>
-          <option value="23">Audio</option>
-          <option value="25">Digital Media</option>
-          <option value="27">Item</option>
-          <option value="24">Moving Image</option>
-          <option value="26">Publication</option>
+          <option value="408">Audio</option>
+          <option value="409">Film/Video</option>
+          <option value="410">Painting</option>
+          <option value="411">Performance</option>
+          <option value="412">Photography</option>
+          <option value="413">Sculpture/Installation/Object</option>
+          <option value="416">Textiles</option>
+          <option value="415">Texts</option>
+          <option value="414">Works on Paper</option>
         </select>
-        <input name="ca_objects.type_id_label" value="Type" type="hidden">
+        <input name="ca_objects_object_category_label" value="Type" type="hidden">
       </div>
       <div class="advanced-search-field">
         <label class="formLabel" for="ca_objects.medium">Medium</label>
         <input class="search-input" name="ca_objects.medium" type="text" id="ca_objects.medium">
-        <input name="ca_objects.medium_label" type="hidden">
+        <input name="ca_objects.medium_label" value="Medium" type="hidden">
       </div>
-      <!-- <div class="advanced-search-field">
-        <label class="formLabel" for="ca_objects.level_description">Level of Description</label>
-        <input class="search-input" name="ca_objects.level_description" type="text"  value="" id="ca_objects_level_description">
-        <input name="ca_objects.level_description" value="" type="hidden">
-      </div> -->
       <div class="advanced-search-field">
         <label class="formLabel" for="ca_objects.idno[]">ID #</label>
-        <input class="search-input" name="ca_objects.idno[]" type="text"  value="" id="ca_objects.idno[]">
-        <input name="ca_objects.idno_label" value="Object identifier" type="hidden">
+        <input class="search-input" name="ca_objects.idno[]" type="text"  value="" id="ca_objects_idno[]">
+        <input name="ca_objects.idno_label" value="ID #" type="hidden">
       </div>
     </div>
 
     <input name="_advancedFormName" value="objects" type="hidden">
-    <input name="_formElements" value="form|_fulltext|ca_objects_catalogue_destination|ca_entities.preferred_labels.displayname|ca_objects.preferred_labels.name|ca_objects.search_date|ca_objects.description|ca_objects.idno|ca_objects.type_id|ca_objects.medium|/form" type="hidden">
+    <input name="_formElements" value="form|_fulltext|ca_objects.catalogue_destination.preferred_labels|ca_entities.preferred_labels.displayname|ca_objects.preferred_labels.name|ca_objects.search_date|ca_objects.description|ca_objects.idno|ca_objects.type_id|ca_objects_object_category|ca_objects.medium|/form" type="hidden">
     <input name="_advanced" value="1" type="hidden">
   </div>
 </form>
